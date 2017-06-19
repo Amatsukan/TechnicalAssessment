d8180a1ac183dbe4d7340cae654497f32cab6581
amatsukan <luiz.cfj@apilab.ufsc.br>
2017-06-19 01:36:58 -0300

    setup for heroku deploy

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
 from pyramid_server import *
 import os
 
-port = 80
-if 'PORT' in os.environ
-	port = os.environ['PORT']
+ON_HEROKU = os.environ.get('ON_HEROKU')
 
+if ON_HEROKU:
+    # get the heroku port
+    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+else:
+    port = 3000
+
+    
 if __name__ == '__main__':
 	run('0.0.0.0', port)
