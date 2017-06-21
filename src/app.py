3f9cfd4a586cbdab3dfdc36bb1b68749c6c0202e
amatsukan <luiz.cfj@apilab.ufsc.br>
2017-06-20 22:59:15 -0300

    Build fix

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ argc = len(sys.argv)
 if argc == 2:
     port = sys.argv[1]
 else:
-	port=3000
+    port=3000
 
 
     """Book Sorter api
+        ./app.py $PORT
     Args:
         Specific port
     """
 
     
 if __name__ == '__main__':
-	print("running in http://localhost:{}".format(port))
-	run('0.0.0.0', int(port))
+    print("running in http://localhost:{}".format(port))
+    run('0.0.0.0', int(port))
--- src/ordered_set.py
+++ src/ordered_set.py
@@ -1,10 +1,5 @@
 import collections
 
-
-   '''
-        OrderedSet
-   '''
-
 class CompareWithNullException(Exception):
     def __init__(self, value):
         self.value = value
